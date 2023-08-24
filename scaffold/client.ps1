5ad22b5dac804fe5d6bdade2076add41bab250c4
Peppe Kerstens <peppekerstens@hotmail.com>
2023-08-24 21:46:31 +0200

    setting up

--- scaffold/client.ps1
+++ scaffold/client.ps1
@@ -1,4 +1,29 @@
-#get logged in
+class League {
+    # Properties
+    [int]$id   #:15
+    [bool]$isInitialized          #: True
+    [array]$seasonIds              #: {58, 59}
+    [string]$subscriptionStatus     #: Unknown
+    [datetime]$createdOn              #: 13-8-2023 07:30:42
+    [datetime]$lastModifiedOn         #: 17-8-2023 16:03:24
+    [string]$createdByUserId        #: 64ce61ed-8f99-4fec-9d5e-e36e576bd1be
+    [string]$lastModifiedByUserId   #: 64ce61ed-8f99-4fec-9d5e-e36e576bd1be
+    [string]$createdByUserName      #: peppekerstens
+    [string]$lastModifiedByUserName #: peppekerstens
+    [string]$name                   #: pec
+    [string]$nameFull               #: Platinum Endurance Championship
+    [string]$description            #:
+    [string]$descriptionPlain       #:
+    [bool]$enableProtests         #: True
+    [timespan]$protestCoolDownPeriod  #: 00:00:00.00000
+    [timespan]$protestsClosedAfter    #: 01.00:00:00.00000
+    [string]$protestsPublic         #: Hidden
+    [string]$leaguePublic           #: PublicListed
+
+    # Constructors
+    League()
+    {} 
+}
 
 
 function Get-LmToken{
@@ -60,7 +85,7 @@ function Get-LmSession{
 function Invoke-LMRestMethod {
     [CmdletBinding(SupportsShouldProcess=$True)] #adds Confirm and WhatIf options as parameter options and so forth 
     param(
-        [ValidateSet('Get','Post')]
+        [ValidateSet('Get','Post','Put','Delete')]
         [string]$Method = 'Get'
         ,
         [parameter(Mandatory=$true)]
@@ -105,7 +130,7 @@ function Invoke-LMRestMethod {
 }
 
 Function Get-LmLeague {
-    [CmdletBinding(SupportsShouldProcess=$True,DefaultParameterSetName = 'None')]
+    [CmdletBinding()]
     param(
         [Parameter(ParameterSetName = 'Name')]
         [string]$Name # without this, returns all
@@ -131,43 +156,78 @@ Function Get-LmLeague {
 }
 
 
-Function New-LmLeague {
-    [CmdletBinding(SupportsShouldProcess=$True,DefaultParameterSetName = 'None')]
+Function LmLeague {
+    [CmdletBinding()]
     param(
-        [Parameter(ParameterSetName = 'Name')]
-        [string]$Name # without this, returns all
+        [Parameter(Mandatory)]
+        [league]$league
         ,
-        [Parameter(ParameterSetName = 'ShortName')]
-        [string]$ShortName # without this, returns all
-        ,
-        [Parameter(ParameterSetName = 'Id')]
-        [string]$id # without this, returns all
+        [ValidateSet('Post','Put','Delete')]
+        [string]$Method
     )
 
-    $EndPoint = "Leagues"
-    if ($id){$EndPoint = "$($EndPoint)/$id"}
-    if ($PSCmdlet.ParameterSetName -eq 'ShortName'){
-        $EndPoint = $shortName
-    }
-    $result = Invoke-LMRestMethod -EndPoint $EndPoint
-    if ($PSCmdlet.ParameterSetName -eq 'Name'){
-        $result.Where{$_.nameFull -eq $Name }
-    }else{
-        $result
-    }
+    If ($Method -eq 'Get'){$EndPoint = "Leagues"}
+    
+    $EndPoint = "Leagues/$($league.id)"
 
-    $body = @{
-        "nameFull": "string",
-        "description": "string",
-        "descriptionPlain": "string",
-        "enableProtests": true,
-        "protestCoolDownPeriod": "01:02:34.56700",
-        "protestsClosedAfter": "01:02:34.56700",
-        "protestsPublic": "Hidden",
-        "leaguePublic": "PublicListed",
-        "name": "string"
-      }
+    $body = @{}
+    if ($name){$body.nameFull = $name}
+    if ($description){$body.description = $description}
+    if ($descriptionPlain){$body.descriptionPlain = $descriptionPlain}
+    if ($enableProtests){$body.enableProtests= $enableProtests}
+    if ($protestCoolDownPeriod){$body.protestCoolDownPeriod = $protestCoolDownPeriod}
+    if ($protestsClosedAfter){$body.protestsClosedAfter = $protestsClosedAfter}
+    if ($protestsPublic){$body.protestsPublic = $protestsPublic}
+    if ($leaguePublic){$body.leaguePublic = $leaguePublic}
+
+    Invoke-LMRestMethod -EndPoint $EndPoint -Method $Method -body $body
+
 }
+
+
+
+Function Update-LmLeague {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$ShortName
+        ,
+        [string]$Name
+        ,
+        [string]$description
+        ,
+        [string]$descriptionPlain
+        ,
+        [switch]$enableProtests
+        ,
+        [timespan]$protestCoolDownPeriod
+        ,
+        [timespan]$protestsClosedAfter
+        ,
+        [ValidatePattern("Hidden")]
+        [string]$protestsPublic
+        ,
+        [ValidatePattern("PublicListed")]
+        [string]$leaguePublic
+    )
+
+    $league = Get-LmLeague -Name $ShortName
+
+    $EndPoint = "Leagues/$($league.id)"
+
+    $body = @{}
+    if ($name){$body.nameFull = $name}
+    if ($description){$body.description = $description}
+    if ($descriptionPlain){$body.descriptionPlain = $descriptionPlain}
+    if ($enableProtests){$body.enableProtests= $enableProtests}
+    if ($protestCoolDownPeriod){$body.protestCoolDownPeriod = $protestCoolDownPeriod}
+    if ($protestsClosedAfter){$body.protestsClosedAfter = $protestsClosedAfter}
+    if ($protestsPublic){$body.protestsPublic = $protestsPublic}
+    if ($leaguePublic){$body.leaguePublic = $leaguePublic}
+
+    Invoke-LMRestMethod -EndPoint $EndPoint -Method Put -body $body
+}
+
 
 Function Get-LmChampionships {
     param(
